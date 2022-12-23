a99d522a59710aceabc13d6dba447f27ea817c8c
Hagen Wierstorf <hwierstorf@audeering.com>
2022-12-23 12:13:55 +0100

    Fix bug

--- docs/conf.py
+++ docs/conf.py
@@ -84,6 +84,7 @@ html_css_files = [
 
 
 # Copy API (sub-)module RST files to docs/api/ folder ---------------------
+audeer.rmdir('api')
 audeer.mkdir('api')
 api_src_files = audeer.list_file_names('api-src')
 api_dst_files = [
