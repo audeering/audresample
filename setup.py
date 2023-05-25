0b36d67270c758d6aa2de531671b21189affe9e2
Hagen Wierstorf <hwierstorf@audeering.com>
2023-05-25 14:35:34 +0200

    Check processor name on windows

--- setup.py
+++ setup.py
@@ -1,44 +1,56 @@
+import os
 import platform
 import setuptools
 
 # Include only the platform specific pre-compiled binary.
 # For sources see https://github.com/audeering/audresamplelib
 
-# Linux
-if platform.system() == 'Linux':
-    path = 'linux/*.so'
-    plat_name = 'manylinux_x86_64'
+binaries = {
+    'manylinux_x86_64': 'linux/*.so',
+    'win_amd64': 'windows/*.dll',
+    'macosx_x86_64': 'macos-intel/*.dylib',
+    'macosx_arm64': 'macos-m1/*.dylib',
+}
 
-# Windows
-elif platform.system() == 'Windows':
-    path = 'windows/*.dll'
-    plat_name = 'win_amd64'
 
-# MacOS Intel
-elif (
-        platform.system() == 'Darwin'
-        and platform.processor() == 'i386'
-):
-    path = 'macos-intel/*.dylib'
-    plat_name = 'macosx_x86_64'
+def platform_name():
 
-# MacOS M1
-elif (
-        platform.system() == 'Darwin'
-        and platform.processor() == 'arm'
-):
-    path = 'macos-m1/*.dylib'
-    plat_name = 'macosx_arm64'
+    # Look for enrionment variable
+    # to be able to enforce
+    # different platform names
+    # in CI on the same runner
+    plat_name = os.environ.get('PLAT_NAME', None)
 
-else:
-    raise RuntimeError('Unsupported platform')
+    if plat_name is None:
 
+        # Extract platform name from system + processor
+        systems = {
+            'Linux': 'manylinux',
+            'Windows': 'win',
+            'Darwin': 'macos',
+        }
+        processors = {
+            'i386': 'x86_64',
+            'arm': 'arm64',
+        }
+        system = platform.system()
+        if system not in systems:
+            raise RuntimeError(f"Unsupported system '{system}'")
+        processor = platform.processor()
+        if processor not in processors:
+            raise RuntimeError(f"Unsupported processor '{processor}'")
+        plat_name = f'{system}_{processor}'
+
+    return plat_name
+
+
+plat_name = platform_name()
 
 setuptools.setup(
     use_scm_version=True,
     packages=setuptools.find_packages(),
     package_data={
-        'audresample.core': [f'bin/{path}']
+        'audresample.core': [f'bin/{binaries[plat_name]}']
     },
     # python -m build --wheel
     # does no longer accept the --plat-name option,
