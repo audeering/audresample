c82f55ae7fd7420a31c3dee3cdbd3fcba0cb1d0b
Hagen Wierstorf <hwierstorf@audeering.com>
2023-05-26 11:19:23 +0200

    Support Raspberry Pi and build platform wheels (#38)
    * Try to build platform wheels
    
    * Try to fix syntax
    
    * Try to force platform wheels
    
    * Try another approach
    
    * Include binary
    
    * Add missing __init__.py file
    
    * Fix data inclusion
    
    * Update build instructions
    
    * Install missing dependencies
    
    * Use python -m build
    
    * Add comment with link
    
    * Fix deprecated license_file
    
    * Show wheels
    
    * Fix syntax
    
    * Check processor name on windows
    
    * Fix building of wheels
    
    * Fix import
    
    * Try to fix env variable
    
    * Include sdist to wheels
    
    * Use newer action versions
    
    * Correct platform name for manylinux
    
    * Fix workflow definition
    
    * FIX: re-enable dist wheel
    
    * Try to fix version
    
    * Another version try
    
    * Specify MacOS version number
    
    * Reuse pltform_name() function
    
    * Look for env variable only in setup.py
    
    * Add Linux aarch64 and arm7l
    
    * Try to fix setup.py
    
    * Build more wheels
    
    * Fix coverage
    
    * Define wheels list differently
    
    * Fix name of MacOS bin folders
    
    * Update tag listing in publish
    
    * Remove wheels test Action

--- audresample/core/lib.py
+++ audresample/core/lib.py
@@ -3,29 +3,62 @@ import os
 import platform
 
 
+def platform_name():
+    r"""Platform name used in pip tag.
+
+    Expected outcomes are:
+
+    ==================== ======================
+    Linux, 64-bit        manylinux_2_17_x86_64
+    Raspberry Pi, 32-bit manylinux_2_17_armv7l
+    Raspberry Pi, 64-bit manylinux_2_17_aarch64
+    Windows              win_amd64
+    MacOS Intel          macosx_12_0_x86_64
+    MacOS M1             macosx_12_0_arm64
+    ==================== ======================
+
+    Under Linux the manylinux version
+    can be extracted
+    by inspecting the wheel
+    with ``auditwheel``.
+
+    Too see all supported tags on your system run:
+
+    .. code-block:: bash
+
+        $ pip debug --verbose
+
+    """
+    system = platform.system()
+    machine = platform.machine().lower()
+    system_mapping = {
+        'Linux': 'manylinux_2_17',
+        'Windows': 'win',
+        'Darwin': 'macosx_12_0',
+    }
+
+    if system not in system_mapping:  # pragma: no cover
+        raise RuntimeError(f'Unsupported platform {system}')
+
+    return f'{system_mapping[system]}_{machine}'
+
+
 # load library
 
 root = os.path.dirname(os.path.realpath(__file__))
-
 bin_path = os.path.join(root, 'bin')
-if platform.system() == 'Windows':  # pragma: no cover
-    lib_path = os.path.join(bin_path, 'windows', 'audresample.dll')
-elif platform.system() == 'Linux':  # pragma: no cover
-    lib_path = os.path.join(bin_path, 'linux', 'libaudresample.so')
-elif (
-        # MacOS Intel
-        platform.system() == 'Darwin'
-        and platform.processor() == 'i386'
-):  # pragma: no cover
-    lib_path = os.path.join(bin_path, 'macos-intel', 'libaudresample.dylib')
-elif (
-        # MacOS M1
-        platform.system() == 'Darwin'
-        and platform.processor() == 'arm'
-):  # pragma: no cover
-    lib_path = os.path.join(bin_path, 'macos-m1', 'libaudresample.dylib')
-else:  # pragma: no cover
-    raise RuntimeError("Unsupported platform")
+
+plat_name = platform_name()
+
+if 'linux' in plat_name:  # pragma: no cover
+    library = 'libaudresample.so'
+elif 'macos' in plat_name:  # pragma: no cover
+    library = 'libaudresample.dylib'
+elif 'win' in plat_name:  # pragma: no cover
+    library = 'audresample.dll'
+
+lib_path = os.path.join(bin_path, plat_name, library)
+
 lib = ctypes.cdll.LoadLibrary(lib_path)
 
 
--- setup.py
+++ setup.py
@@ -1,4 +1,77 @@
-from setuptools import setup
+import os
+import platform
+
+import setuptools
 
 
-setup(use_scm_version=True)
+# Include only the platform specific pre-compiled binary.
+# For sources see https://github.com/audeering/audresamplelib
+
+
+def platform_name():
+    r"""Platform name used in pip tag.
+
+    Expected outcomes are:
+
+    ==================== ======================
+    Linux, 64-bit        manylinux_2_17_x86_64
+    Raspberry Pi, 32-bit manylinux_2_17_armv7l
+    Raspberry Pi, 64-bit manylinux_2_17_aarch64
+    Windows              win_amd64
+    MacOS Intel          macosx_12_0_x86_64
+    MacOS M1             macosx_12_0_arm64
+    ==================== ======================
+
+    Under Linux the manylinux version
+    can be extracted
+    by inspecting the wheel
+    with ``auditwheel``.
+
+    Too see all supported tags on your system run:
+
+    .. code-block:: bash
+
+        $ pip debug --verbose
+
+    """
+    system = platform.system()
+    machine = platform.machine().lower()
+    system_mapping = {
+        'Linux': 'manylinux_2_17',
+        'Windows': 'win',
+        'Darwin': 'macosx_12_0',
+    }
+
+    if system not in system_mapping:
+        raise RuntimeError(f'Unsupported platform {system}')
+
+    return f'{system_mapping[system]}_{machine}'
+
+
+# Look for enrionment variable PLAT_NAME
+# to be able to enforce
+# different platform names
+# in CI on the same runner
+plat_name = os.environ.get('PLAT_NAME', platform_name())
+
+if 'linux' in plat_name:
+    library = '*.so'
+elif 'macos' in plat_name:
+    library = '*.dylib'
+elif 'win' in plat_name:
+    library = '*.dll'
+
+setuptools.setup(
+    use_scm_version=True,
+    packages=setuptools.find_packages(),
+    package_data={
+        'audresample.core': [f'bin/{plat_name}/{library}']
+    },
+    # python -m build --wheel
+    # does no longer accept the --plat-name option,
+    # but we can set the desired platform as an option
+    # (https://stackoverflow.com/a/75010995)
+    options={
+        'bdist_wheel': {'plat_name': plat_name},
+    },
+)
